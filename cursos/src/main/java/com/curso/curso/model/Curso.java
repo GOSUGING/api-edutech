0d3b13540249e6dd7ed79da7b54ac02770fabf05
GOSUGING <aroon.suy@gmail.com>
2025-05-29 10:24:22 -0400

    quick fix

---
+++ cursos/src/main/java/com/curso/curso/model/Curso.java
@@ -0,0 +1,39 @@
+package com.curso.curso.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "curso")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+
+public class Curso {
+
+    @Id
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nombreCurso;
+
+    @Column
+    private String descripcionCurso;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE) // Esto indica que solo se guarda la fecha, no la hora
+    private Date duracionCurso;
+
+    @Column(name = "estado_curso_id", nullable = false)
+    private String estadoCurso;
+
+}
